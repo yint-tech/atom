dfbe4bf2748fcaf230538b8b17a90cc5acb27817
iinti_cn <iinti@iinti.cn>
2024-04-16 09:11:15 +0800

    upgrade vuepress version

--- atom-backend/doc/src/.vuepress/config.ts
+++ atom-backend/doc/src/.vuepress/config.ts
@@ -1,6 +1,7 @@
 import {defineUserConfig} from 'vuepress'
 import {head} from "./configs";
 import theme from "./theme.js";
+import viteBundler from "@vuepress/bundler-vite";
 
 export default defineUserConfig({
     base: "/atom-doc/",
@@ -8,5 +9,6 @@ export default defineUserConfig({
     lang: 'zh-CN',
     title: 'atom',
     description: 'java单体全栈开发脚手架',
+    bundler: viteBundler(),
     theme
 })
