88047957a5151aad90345104ecd884661413234f
iinti_cn <iinti@iinti.cn>
2024-12-11 12:47:00 +0800

    feat: aliyun repository mirror

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,12 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     repositories {
+        maven {
+            setUrl("https://maven.aliyun.com/repository/public")
+        }
+        maven {
+            setUrl("https://maven.aliyun.com/repository/gradle-plugin")
+        }
         mavenCentral()
         gradlePluginPortal()
     }
@@ -10,6 +16,12 @@ pluginManagement {
 
 dependencyResolutionManagement {
     repositories {
+        maven {
+            setUrl("https://maven.aliyun.com/repository/public")
+        }
+        maven {
+            setUrl("https://maven.aliyun.com/repository/google")
+        }
         mavenCentral()
     }
 }
